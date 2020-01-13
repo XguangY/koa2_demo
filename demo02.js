db5d9019c3c7a86e4e7c3c975de40983b8bf6905
theone <xuguangyue@dtrobo.com>
2020-01-13 16:56:16 +0800

    koa-bodyparser 中间件的使用

--- demo02.js
+++ demo02.js
@@ -1,6 +1,7 @@
 const koa = require('koa')
-
+const bodyParser = require('koa-bodyparser')
 const app = new koa()
+app.use(bodyParser())
 
 app.use(async (ctx) => {
     //当请求时GET请求时，显示表单让用户填写
@@ -20,7 +21,8 @@ app.use(async (ctx) => {
         ctx.body = html;
     } else if (ctx.url === '/' && ctx.method === 'POST') {
         //当请求时POST请求时
-        ctx.body = await parsePostData(ctx)
+        // ctx.body = await parsePostData(ctx)
+        ctx.body = ctx.request.body
     } else {
         // 其他404
         ctx.body = `404`
@@ -32,33 +34,33 @@ app.listen(9999, () => {
 })
 
 
-// 处理post 请求参数
+// // 处理post 请求参数
 
-function parsePostData(ctx) {
-    return new Promise((resolve, reject) => {
-        try {
-            let postData = ''
-            // 监听data数据
-            ctx.req.addListener('data', (data) => {
-                postData += data
-            })
-            ctx.req.on('end', () => {
-                resolve(parseQueryStr(postData))
-            })
-        } catch(error){
-            reject(error)
-        }
-    })
-}
+// function parsePostData(ctx) {
+//     return new Promise((resolve, reject) => {
+//         try {
+//             let postData = ''
+//             // 监听data数据
+//             ctx.req.addListener('data', (data) => {
+//                 postData += data
+//             })
+//             ctx.req.on('end', () => {
+//                 resolve(parseQueryStr(postData))
+//             })
+//         } catch(error){
+//             reject(error)
+//         }
+//     })
+// }
 
-// 将post 的数据格式化为数组
+// // 将post 的数据格式化为数组
 
-function parseQueryStr (str) {
-    let queryData = {}
-    let strArrList = str.split('&')
-    strArrList.map(item => {
-        let itemArr = item.split('=')
-        queryData[itemArr[0]] = decodeURIComponent(itemArr[1])
-    })
-    return queryData
-}
+// function parseQueryStr (str) {
+//     let queryData = {}
+//     let strArrList = str.split('&')
+//     strArrList.map(item => {
+//         let itemArr = item.split('=')
+//         queryData[itemArr[0]] = decodeURIComponent(itemArr[1])
+//     })
+//     return queryData
+// }
