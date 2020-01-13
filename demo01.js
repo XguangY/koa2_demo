d1d1599c56d9614df15d1de09c75596ab43e33c9
theone <xuguangyue@dtrobo.com>
2020-01-13 15:08:33 +0800

    获取GET请求的两种方式

---
+++ demo01.js
@@ -0,0 +1,34 @@
+const koa = require('koa')
+
+const app = new koa()
+
+app.use(async (ctx) => {
+    const url = ctx.url
+
+
+    // request 获取 query
+    const req = ctx.request
+    // query：返回的是格式化好的参数对象。
+    const req_query = req.query
+    // querystring：返回的是请求字符串
+    const req_querystring = req.querystring
+    
+
+    // 直接从ctx 中获取
+    const req_query1 = req.query
+    const req_querystring1 = req.querystring
+
+
+    ctx.body = {
+        url,
+        req_query,
+        req_querystring,
+        req_query1,
+        req_querystring1
+    }
+})
+
+// 监听
+app.listen(9999, () => {
+    console.log('port 9999')
+})
